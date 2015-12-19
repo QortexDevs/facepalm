c7181bd9a115e2111cac372cd356eb36517aa0e3
Mikhail Lisnyak <xpundel@gmail.com>
2015-12-19 22:45:02 +0300

    minor changes

--- app/Facepalm/Fields/FieldFactory.php
+++ app/Facepalm/Fields/FieldFactory.php
@@ -14,17 +14,6 @@ use Illuminate\Support\Str;
 
 class FieldFactory
 {
-
-    const TYPE_STRING = 'string';
-    const TYPE_DATE = 'date';
-    const TYPE_DATETIME = 'datetime';
-    const TYPE_TEXT = 'text';
-    const TYPE_INTEGER = 'integer';
-    const TYPE_DICTIONARY = 'dictionary';
-    const TYPE_RELATION = 'relation';
-    const TYPE_PASSWORD = 'password';
-    const TYPE_BOOLEAN = 'boolean';
-
     /**
      * todo: подумать, как использовать ДИ-контейнер, чтобыне напрямую создавать, а через сервисы, чтоб можно было подменить
      * @param $type
@@ -33,6 +22,10 @@ class FieldFactory
      */
     public function get($type, $params = [])
     {
+        //todo: aliases, instead of hardcode
+        if ($type == 'checkbox') {
+            $type = 'boolean';
+        }
         $className = '\App\Facepalm\Fields\Types\\' . Str::ucfirst($type) . 'Field';
         if (class_exists($className)) {
             return new $className($params);
