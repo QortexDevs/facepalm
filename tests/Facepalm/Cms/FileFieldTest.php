a4df88dbcbabfa21bea603f61d58263a34fd64db
Mikhail Lisnyak <xpundel@gmail.com>
2016-02-05 18:07:42 +0300

    Image and file fields refactoring and test

---
+++ tests/Facepalm/Cms/FileFieldTest.php
@@ -0,0 +1,30 @@
+<?php
+namespace Tests;
+
+use App\Facepalm\Cms\Fields\FieldFactory;
+use App\Facepalm\Cms\Fields\Types\BooleanField;
+use App\Facepalm\Cms\Fields\Types\DateField;
+use App\Facepalm\Cms\Fields\Types\DatetimeField;
+use App\Facepalm\Cms\Fields\Types\DictionaryField;
+use App\Facepalm\Cms\Fields\Types\FileField;
+use App\Facepalm\Cms\Fields\Types\ImageField;
+use App\Facepalm\Cms\Fields\Types\IntegerField;
+use App\Facepalm\Cms\Fields\Types\PasswordField;
+use App\Facepalm\Cms\Fields\Types\RelationField;
+use App\Facepalm\Cms\Fields\Types\SelectField;
+use App\Facepalm\Cms\Fields\Types\StringField;
+use App\Facepalm\Cms\Fields\Types\TextField;
+use App\Facepalm\Cms\Fields\Types\UnknownField;
+
+class FileFieldTest extends TestCase
+{
+    public function testSkippedForNonObject()
+    {
+        $field = (new FileField())->setName('docs');
+        $field->prepareData();
+        $this->assertTrue($field->isSkipped());
+        //todo: make mocks and test prepareData
+    }
+
+
+}
