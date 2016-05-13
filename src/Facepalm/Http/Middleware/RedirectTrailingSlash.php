e591062a4d42d373085194ad5deb902deb12acd2
Mikhail Lisnyak <xpundel@gmail.com>
2016-05-13 18:13:23 +0300

    bugfix in trailing slash

--- src/Facepalm/Http/Middleware/RedirectTrailingSlash.php
+++ src/Facepalm/Http/Middleware/RedirectTrailingSlash.php
@@ -18,13 +18,12 @@ class RedirectTrailingSlash
     {
         if ($request->method() === 'GET') {
             $url = $request->getBaseUrl() . $request->getPathInfo();
-            if (!preg_match('/.+\/$/', $url)) {
-
+            if ($url && $url !== '/' && !preg_match('/.+\/$/', $url)) {
                 if (null !== $qs = $request->getQueryString()) {
                     $qs = '?' . $qs;
                 }
 
-                // Built-in laravel redirector always trim slash
+                // Built-in laravel redirector always trim slash, so we use plain header()
                 header('Location:' . $url . '/' . $qs, 301);
                 exit;
             }
