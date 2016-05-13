48255549ed3bbc47f4ba8e1e5af4edd06b657f8d
Mikhail Lisnyak <xpundel@gmail.com>
2016-05-13 16:05:54 +0300

    slash redirecotr middleware

---
+++ src/Facepalm/Http/Middleware/RedirectTrailingSlash.php
@@ -0,0 +1,36 @@
+<?php namespace Facepalm\Http\Middleware;
+
+use Closure;
+use Illuminate\Support\Facades\Redirect;
+use Illuminate\Support\Str;
+
+class RedirectTrailingSlash
+{
+
+    /**
+     * Check if there is no trailing slash and redirect to slashed-variant
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if ($request->method() === 'GET') {
+            $url = $request->getBaseUrl() . $request->getPathInfo();
+            if (!preg_match('/.+\/$/', $url)) {
+
+                if (null !== $qs = $request->getQueryString()) {
+                    $qs = '?' . $qs;
+                }
+
+                // Built-in laravel redirector always trim slash
+                header('Location:' . $url . '/' . $qs, 301);
+                exit;
+            }
+        }
+
+        return $next($request);
+    }
+
+}
