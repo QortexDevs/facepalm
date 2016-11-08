66fc36cf818b0732bde6613f3e7c9603aaba1bbe
Mikhail Lisnyak <xpundel@gmail.com>
2016-11-08 13:07:13 +0300

    pluralize function

--- src/Facepalm/helpers.php
+++ src/Facepalm/helpers.php
@@ -42,3 +42,30 @@ if (!function_exists('pre')) {
 
     }
 }
+
+if (!function_exists('pluralize')) {
+    function pluralize($value, $strings)
+    {
+        $value = round($value);
+        if ($value > 100) {
+            $value = $value % 100;
+        }
+
+        $firstDigit = $value % 10;
+        $secondDigit = floor($value / 10);
+
+        if ($secondDigit != 1) {
+            if ($firstDigit == 1) {
+                return $strings[0];
+            } else {
+                if ($firstDigit > 1 && $firstDigit < 5) {
+                    return $strings[1];
+                } else {
+                    return $strings[2];
+                }
+            }
+        } else {
+            return $strings[2];
+        }
+    }
+}
